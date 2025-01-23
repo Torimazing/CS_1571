90b07a48b76907bf80483f1b47920af3b95fce91
Torimazing <112402710+Torimazing@users.noreply.github.com>
2025-01-23 17:44:07 -0500

    bfs stats

--- Pitt AI Spring 25 HW1/bfs_stats.py
+++ Pitt AI Spring 25 HW1/bfs_stats.py
@@ -14,19 +14,34 @@ from Puzzle8 import *
  #### ++++++++++++++++++++++++++++++++++++++++++++++++++++
  #### breadth first search         
         
+global expanded_count
+global generated_count
+global max_len
+global solution_len
+
+expanded_count = 0
+generated_count = 0
+max_len = 0
+solution_len = 0
 
 def breadth_first_search(problem):
+     global expanded_count, generated_count, max_len, solution_len
      queue =deque([])
      root=TreeNode(problem,problem.initial_state)
      queue.append(root)   
      while len(queue)>0:
+         if len(queue) > max_len:
+             max_len = len(queue)
+         expanded_count += 1
          next=queue.popleft()
          if next.goalp():
              del(queue)
+             solution_len = len(next.path())
              return next.path()
          else:
              new_nodes=next.generate_new_tree_nodes()
              for new_node in new_nodes:
+                  generated_count += 1
                   queue.append(new_node)         
      print('No solution')
      return NULL
@@ -36,6 +51,11 @@ problem=Puzzle8_Problem(Example1)
 output=  breadth_first_search(problem)
 print('Solution Example 1:')
 print_path(output)
+print("Nodes Expanded: "+str(expanded_count) + "\nNodes Generated: "+ str(generated_count) + "\nMax Queue Length: " + str(max_len) + "\nSolution Length: " + str(solution_len))
+expanded_count = 0
+generated_count = 0
+max_len = 0
+solution_len = 0
 
 wait = input("PRESS ENTER TO CONTINUE.")
 
@@ -43,6 +63,11 @@ problem=Puzzle8_Problem(Example2)
 output=  breadth_first_search(problem)
 print('Solution Example 2:')
 print_path(output)
+print("Nodes Expanded: "+str(expanded_count) + "\nNodes Generated: "+ str(generated_count) + "\nMax Queue Length: " + str(max_len) + "\nSolution Length: " + str(solution_len))
+expanded_count = 0
+generated_count = 0
+max_len = 0
+solution_len = 0
 
 wait = input("PRESS ENTER TO CONTINUE.")
 
@@ -50,6 +75,11 @@ problem=Puzzle8_Problem(Example3)
 output=  breadth_first_search(problem)
 print('Solution Example 3:')
 print_path(output)
+print("Nodes Expanded: "+str(expanded_count) + "\nNodes Generated: "+ str(generated_count) + "\nMax Queue Length: " + str(max_len) + "\nSolution Length: " + str(solution_len))
+expanded_count = 0
+generated_count = 0
+max_len = 0
+solution_len = 0
 
 wait = input("PRESS ENTER TO CONTINUE.")
 
@@ -57,6 +87,11 @@ problem=Puzzle8_Problem(Example4)
 output=  breadth_first_search(problem)
 print('Solution Example 4:')
 print_path(output)
+print("Nodes Expanded: "+str(expanded_count) + "\nNodes Generated: "+ str(generated_count) + "\nMax Queue Length: " + str(max_len) + "\nSolution Length: " + str(solution_len))
+expanded_count = 0
+generated_count = 0
+max_len = 0
+solution_len = 0
 
 # Solution to Example 5 may take too long to calculate using vanilla bfs
 # problem=Puzzle8_Problem(Example5) 
